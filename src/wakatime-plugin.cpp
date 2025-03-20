c20edd1c9dca092bf3b8798e5c0ba30da5352c99
jenskromdijk <jenskromdijk@gmail.com>
2025-03-20 18:30:34 +0000

    Hook up documentController signals to event slots

--- src/wakatime-plugin.cpp
+++ src/wakatime-plugin.cpp
@@ -4,6 +4,9 @@
 
 #include <KPluginFactory>
 
+#include <interfaces/idocumentcontroller.h>
+#include <interfaces/iprojectcontroller.h>
+
 K_PLUGIN_FACTORY_WITH_JSON(wakatime_pluginFactory, "wakatime-plugin.json", registerPlugin<WakatimePlugin>(); )
 
 WakatimePlugin::WakatimePlugin(QObject* parent, const KPluginMetaData& metaData, const QVariantList& args)
@@ -11,13 +14,43 @@ WakatimePlugin::WakatimePlugin(QObject* parent, const KPluginMetaData& metaData,
 {
     Q_UNUSED(args);
 
-    qCDebug(PLUGIN_WAKATIME_PLUGIN) << "Hello world, my plugin is loaded!";
+    qCDebug(PLUGIN_WAKATIME_PLUGIN) << "Wakatime plugin is loaded!";
 }
 
-WakatimePlugin::~WakatimePlugin() noexcept
+WakatimePlugin::~WakatimePlugin()
 {
 }
 
+void WakatimePlugin::addListeners()
+{
+    // connect documentController signals to WakatimePlugin slots
+    connect(KDevelop::ICore::self()->documentController(),
+            &KDevelop::IDocumentController::documentJumpPerformed,
+            this,
+            &WakatimePlugin::documentModified);
+    connect(KDevelop::ICore::self()->documentController(),
+            &KDevelop::IDocumentController::documentClosed,
+            this,
+            &WakatimePlugin::documentClosed);
+    connect(KDevelop::ICore::self()->documentController(),
+            &KDevelop::IDocumentController::documentOpened,
+            this,
+            &WakatimePlugin::documentOpened);
+    connect(KDevelop::ICore::self()->documentController(),
+            &KDevelop::IDocumentController::documentSaved,
+            this,
+            &WakatimePlugin::documentSaved);
+    connect(KDevelop::ICore::self()->documentController(),
+            &KDevelop::IDocumentController::documentLoaded,
+            this,
+            &WakatimePlugin::documentSwitched);
+}
+
+WakatimePluginView::WakatimePluginView(WakatimePlugin* plugin)
+ : m_wakatimePlugin{plugin}
+{
+
+}
 
 // needed for QObject class created from K_PLUGIN_FACTORY_WITH_JSON
 #include "wakatime-plugin.moc"
--- src/wakatime-plugin.h
+++ src/wakatime-plugin.h
@@ -2,6 +2,10 @@
 #define WAKATIME_PLUGIN_H
 
 #include <interfaces/iplugin.h>
+#include <interfaces/icore.h>
+#include <interfaces/idocument.h>
+
+#define KDEV_WAKATIME_PLUGIN_VERSION "1.0.0"
 
 class WakatimePlugin : public KDevelop::IPlugin
 {
@@ -12,6 +16,28 @@ public:
     WakatimePlugin(QObject* parent, const KPluginMetaData& metaData, const QVariantList& args);
 
     ~WakatimePlugin();
+
+    // connect KDevelop::documentController signals to Q_SLOTS
+    void addListeners();
+
+public Q_SLOTS:
+    // document events
+    void documentSwitched(void* document);
+    void documentModified(void* document);
+    void documentClosed(void* document);
+    void documentSaved(void* document);
+    void documentOpened(void* document);
+};
+
+class WakatimePluginView : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit WakatimePluginView(WakatimePlugin* plugin);
+
+private:
+    WakatimePlugin* m_wakatimePlugin {nullptr};
 };
 
 #endif // WAKATIME_PLUGIN_H
